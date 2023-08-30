b9382966cc691b5da05939bc1342e9069fb7197c
Love <greger.grym@gmail.com>
2023-08-30 11:28:49 +0200

    coin manager

---
+++ My project/Assets/Coin manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Coinmanager : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject cash;
+
+    void Update()
+    {
+        if(cash == null) {
+            SpawnCoin();
+        }
+    }
+
+    private void SpawnCoin(){
+        float x = Random.Range(-10f,10f);
+        float z = Random.Range(-10f,10f);
+        Vector3 position = new Vector3(x,1.5f,z);
+
+        cash = Instantiate(prefab);
+        cash.transform.position = position;
+    }
+}
